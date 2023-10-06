71e35d568fddf0d85991634cf838d1a1d3f5fbd5
João Victor da Silva <jv782063@gmail.com>
2023-10-05 23:45:05 -0300

    connection database

---
+++ internal/infra/database/sqlite.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+)
+
+type Connection interface {
+  Connect() (*sql.DB, error)
+}
+
+func Connect() (*sql.DB, error) {
+  db, err := sql.Open("sqlite3", "./database.db")
+  if err != nil {
+    log.Fatalln("Error on connect to database:\n", err)
+    return nil, err
+  }
+
+  if err = db.Ping(); err != nil {
+    log.Fatalln("Error on ping to database:\n", err)
+    return nil, err
+  }
+
+  return db, nil
+}
+
